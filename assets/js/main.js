4cce14ef1a9ea33a52f1aba886fd791bb7fe75ad
antonio <antoniorocca46@gmail.com>
2020-11-30 21:37:02 +0100

    Final Commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$('#tariffa').mouseenter(function () { 
+    $('#menu-nascosto2').toggle(100) 
+});
+$('#tariffa').mouseleave(function () { 
+    $('#menu-nascosto2').toggle(100) 
+});
+$('#conti').mouseenter(function () { 
+    $('#menu-nascosto3').toggle(100) 
+});
+$('#conti').mouseleave(function () { 
+    $('#menu-nascosto3').toggle(100) 
+});
+$('#prodotti').mouseenter(function () { 
+    $('#menu-3col').toggle(100) 
+});
+$('#prodotti').mouseleave(function () { 
+    $('#menu-3col').toggle(100) 
+});
+$('#azienda').mouseenter(function () { 
+    $('#menu-nascosto1').toggle(100) 
+});
+$('#azienda').mouseleave(function () { 
+    $('#menu-nascosto1').toggle(100) 
+});
+$('#aiuto').mouseenter(function () { 
+    $('#menu-nascosto4').toggle(100) 
+});
+$('#aiuto').mouseleave(function () { 
+    $('#menu-nascosto4').toggle(100) 
+});
